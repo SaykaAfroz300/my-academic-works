bd46bfc4135a0ed3887cc8bcce5da329b60adcf8
itsmetechno <afrozsaika2002@gmail.com>
2026-01-07 20:01:53 +0600

    Initial clean commit

---
+++ CPUSchedulingApp/resource.h
@@ -0,0 +1,36 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by CPUSchedulingApp.rc
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_CPUSCHEDULINGAPP_DIALOG     102
+#define IDR_MAINFRAME                   128
+#define IDC_BUTTON_FCFS                 1002
+
+#define IDC_BUTTON4_priority2           1004
+#define IDC_BUTTON7_SJN                 1006
+#define IDC_LIST1                       1012
+#define IDC_EDIT5                       1014
+#define IDC_EDIT6                       1015
+#define IDC_EDIT7                       1016
+#define IDC_BUTTON_ADD_PROCESS          1018
+#define IDC_BUTTON11_Clear              1019
+#define IDC_EDIT2_Waiting               1021
+#define IDC_EDIT3_Turnaround            1022
+#define IDC_EDIT4_Completion            1023
+#define IDC_EDIT1_Result                1024
+#define IDC_BUTTON2_SJF_preemptive      1026
+
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1037
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
