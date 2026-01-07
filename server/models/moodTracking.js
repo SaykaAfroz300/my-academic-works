0cda1eb0b02feb3db8db9dbb2913ffeaeda9dc25
SaykaAfroz300 <jellymango066@gmail.com>
2026-01-07 20:35:52 +0600

    Delete server directory

--- server/models/moodTracking.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-const moodTrackingSchema = require("../Schema/moodTrackingSchema");
-
-const MoodTracking = mongoose.model("MoodTracking", moodTrackingSchema);
-
-module.exports = MoodTracking;
